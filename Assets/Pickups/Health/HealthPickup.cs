f276ef0450f2da74f7ceee766e1e008b90d93a53
WaterKat <waterkatgames@gmail.com>
2020-03-16 22:17:45 -0700

    Pickups (Health)

---
+++ Assets/Pickups/Health/HealthPickup.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using WaterKat.Player_N;
+
+namespace WaterKat.Pickups
+{
+    public class HealthPickup : MonoBehaviour
+    {
+        public int HealAmount = 1;
+        private void OnTriggerEnter(Collider collider)
+        {
+            PlayerHealth touchingPlayerHealth = collider.GetComponent<PlayerHealth>();
+
+            if (touchingPlayerHealth == null) { return; }
+
+            touchingPlayerHealth.curHealth += HealAmount;
+
+            Debug.Log("Healing: " + HealAmount);
+
+            Destroy(this.gameObject);
+        }
+    }
+}
