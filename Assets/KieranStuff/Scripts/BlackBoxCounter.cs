e314f939bf45f1e413b40e4d13b43e31d5d526ed
kizido <kieranking3555@gmail.com>
2020-03-31 22:59:27 -0700

    added game win / loss panel and fixed swam drone missile instantiation bug

---
+++ Assets/KieranStuff/Scripts/BlackBoxCounter.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+using WaterKat.Player_N;
+
+public class BlackBoxCounter : MonoBehaviour
+{
+    [SerializeField]
+    private TextMeshProUGUI blackBoxText;
+    [SerializeField]
+    private GameObject winGamePanel;
+    private CameraController camController;
+
+    private int _blackBoxCount = 0;
+    private int blackBoxGoal = 4;
+    public int blackBoxCount
+    {
+        get
+        {
+            return _blackBoxCount;
+        }
+        set
+        {
+            _blackBoxCount = value;
+            UpdateBlackBoxUI();
+            if(_blackBoxCount >= blackBoxGoal)
+            {
+                WinGame();
+            }
+        }
+    }
+
+    private void Start()
+    {
+        camController = GetComponent<CameraController>();
+        UpdateBlackBoxUI();
+    }
+
+    private void UpdateBlackBoxUI()
+    {
+        blackBoxText.text = blackBoxCount + " Boxes Collected";
+    }
+
+    private void WinGame()
+    {
+        camController.enabled = false;
+        Time.timeScale = 0;
+        winGamePanel.SetActive(true);
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+    public void PlayAgain()
+    {
+        Scene scene = SceneManager.GetActiveScene();
+        Time.timeScale = 1;
+        SceneManager.LoadScene(scene.name);
+    }
+
+}
--- Assets/KieranStuff/Scripts/PlayerHealth.cs
+++ Assets/KieranStuff/Scripts/PlayerHealth.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using WaterKat.Player_N;
 
 public class PlayerHealth : MonoBehaviour
 {
@@ -8,6 +9,12 @@ public class PlayerHealth : MonoBehaviour
     public int maxHealth = 10;
     [SerializeField]
     private int _curHealth = 10;
+    public CameraShake cameraShake;
+
+
+    private CameraController camController;
+    [SerializeField]
+    private GameObject loseGamePanel;
     
     public int curHealth
     {
@@ -17,8 +24,18 @@ public class PlayerHealth : MonoBehaviour
         }
         set 
         {
+            // Shake screen if lose health
+            if(curHealth > value)
+            {
+                StartCoroutine(cameraShake.SingleShake(0.05f, 0.02f));
+            }
+
             _curHealth = Mathf.Clamp(value, 0, maxHealth);
             playerHealthText.UpdateHealthUI();
+            if (_curHealth <= 0)
+            {
+                LoseGame();
+            }
         }
     }
 
@@ -30,5 +47,18 @@ public class PlayerHealth : MonoBehaviour
     public void TakeDamage(int damage)
     {
         curHealth -= damage;
+    }
+    private void LoseGame()
+    {
+        camController.enabled = false;
+        Time.timeScale = 0;
+        loseGamePanel.SetActive(true);
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
+    private void Start()
+    {
+        camController = GetComponent<CameraController>();
     }
 }
