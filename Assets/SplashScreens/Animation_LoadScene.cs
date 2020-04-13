d9b6e2e93c0b96ea1a443a8785f6d429684c6e9d
WaterKat <waterkatgames@gmail.com>
2020-04-12 22:34:28 -0700

    AddingSplashScreens

---
+++ Assets/SplashScreens/Animation_LoadScene.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animation_LoadScene : MonoBehaviour
+{
+    public string desiredScene = "SplashScreens";
+    public void LoadDesiredScene()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(desiredScene);
+    }
+}
