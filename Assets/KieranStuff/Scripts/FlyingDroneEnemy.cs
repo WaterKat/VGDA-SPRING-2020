b91a1cc7d5d4c51348b9983b8f6993b44362df73
kizido <kieranking3555@gmail.com>
2020-04-14 23:41:08 -0700

    started rework of drone

--- Assets/KieranStuff/Scripts/FlyingDroneEnemy.cs
+++ Assets/KieranStuff/Scripts/FlyingDroneEnemy.cs
@@ -1,18 +1,94 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class FlyingDroneEnemy : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private Rigidbody rb;
+    [SerializeField]
+    private Transform playerTrans;
+
+    private Vector3 originPos;
+    private Vector3 randWanderPos;
+    private float wanderSpeed = 5f;
+    private float wanderRadius = 10f;
+    private bool aiWalking = true;
+
+    bool playerAggro = false;
+    float aggroDistance = 50f;
+
+    [Header("Missile Stuff")]
+    public GameObject missile;
+    public Transform missileSpawnLoc;
+    public float missileForce = 100f;
+    public float attackDelay = 3f;
+    private bool missileReady = true;
+
+    private void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        playerTrans = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+
+        originPos = this.transform.position;
+        randWanderPos = new Vector3(originPos.x + Random.Range(-wanderRadius, wanderRadius), originPos.y, originPos.z + Random.Range(-wanderRadius, wanderRadius));
+        this.transform.LookAt(randWanderPos);
+
+        Debug.Log("ORIGIN: " + originPos);
+        Debug.Log("RANDOM: " + randWanderPos);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        if(playerAggro == false)
+        {
+            BasicAI();
+        }
+        else
+        {
+            FollowPlayer();
+        }
+    }
+
+    void ShootMissile()
+    {
+        Debug.Log("MISSILE SHOT");
+        GameObject tempMissile = Instantiate(missile, missileSpawnLoc.position, this.transform.rotation) as GameObject;
+        Rigidbody tempMissileRb = tempMissile.GetComponent<Rigidbody>();
+        StartCoroutine(ReloadMissile());
+    }
+
+    IEnumerator ReloadMissile()
+    {
+        missileReady = false;
+        yield return new WaitForSeconds(attackDelay + Random.Range(-0.5f, 0.5f));
+        missileReady = true;
+    }
+
+    private void BasicAI()
+    {
+        if ((rb.position - randWanderPos).magnitude < 1f && aiWalking) {
+            StartCoroutine(ChooseNewPosAndWait());
+        }
+        else
+        {
+            if(aiWalking)
+            {
+                rb.MovePosition(this.transform.position + ((randWanderPos - this.transform.position).normalized * wanderSpeed * Time.fixedDeltaTime));
+            }
+        }
+    }
+
+    private void FollowPlayer()
+    {
+
+    }
+
+    private IEnumerator ChooseNewPosAndWait()
+    {
+        aiWalking = false;
+        yield return new WaitForSeconds(2f);
+        randWanderPos = new Vector3(originPos.x + Random.Range(-wanderRadius, wanderRadius), originPos.y, originPos.z + Random.Range(-wanderRadius, wanderRadius));
+        this.transform.LookAt(randWanderPos);
+        aiWalking = true;
     }
 }
